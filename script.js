53fdf0ad7f34290e47a1f9ceb7ac2c211a065d67
annakv831 <annakoley1000@gmail.com>
2021-10-27 19:45:54 +0300

    change title

---
+++ script.js
@@ -0,0 +1,21 @@
+let age;
+let userName;
+let userSurname;
+
+age = 42;
+userName = 'Anna';
+userSurname = 'Kolej';
+
+let newUser;
+let listShop;
+let isUserOnline;
+let workersSalary;
+let coordCursorMouse;
+
+let word ='Hello, World!';
+console.log(word);
+
+newUser ='John';
+
+console.log('newUser-', newUser);
+console.log('age-',age);
